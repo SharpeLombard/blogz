d331a029034eb6bd917d90a42b355fdbd73838d8
Mad Hatter <rebecca.s.lombard@gmail.com>
2018-04-16 15:51:55 -0500

    Getting ready for an overhaul

--- main.py
+++ main.py
@@ -29,29 +29,29 @@ def get_bloglist():
 #/add is where we go after hitting Add New
 @app.route("/add", methods=['GET'])
 def new_blog_text():
-   # encoded_error = request.args.get("error1")
+    #encoded_error = request.args.get("error1")
     #next_error = request.args.get("error2")
     return render_template('Add-New_Blog.html')
 
 #/Newpost is where we go after hitting 'Add It'
-@app.route("/newpost", methods=['POST'])
+@app.route("/newpost", methods=['POST','GET'])
 def add_blog():
-    new_blog = request.form['new_blog']
-    blog_text = request.form['blog_text']
-    #blog_id = int(request.form['blog.id'])
-    title_error = ''
-    body_error = ''    
+    if request.method == 'POST':
+        new_blog = request.form['new_blog']
+        blog_text = request.form['blog_text']
+        title_error = ''
+        body_error = ''    
 
     #if len(new_blog_text)==0 and len(new_blog_title)==0:
      #   title-error = "Please enter blog title."
       #  body-error = "Please enter blog text." 
 
-    if len(blog_text)==0:
+    if not blog_text:
         body_error = "Please enter blog text."
         blog_text = blog_text
         new_blog = new_blog
 
-    if len(new_blog)==0:
+    if not new_blog:
         title_error = "Please enter blog title."
         blog_text = blog_text
         new_blog = new_blog
@@ -60,18 +60,24 @@ def add_blog():
         blog = Blog(new_blog,blog_text)
         db.session.add(blog)
         db.session.commit()
-        return render_template('new-post.html', blog=blog)
+        return redirect('/blog?id={}'.format(blog.id))
     else:
-        new_blog = request.form['new_blog']
-        blog_text = request.form['blog_text']
+        #new_blog = request.form['new_blog']
+        #blog_text = request.form['blog_text']
         return render_template('Add-New_Blog.html',blog_text=blog_text, new_blog=new_blog,
         body_error=body_error,title_error=title_error)
+    return render_template('Add-New_Blog.html', title='New Post')
 
-@app.route("/blog", methods=['POST','GET'])
+@app.route("/blog")
 def viewblog():
-    blog_id = int(request.form['blog.id'])
-    blog = Blog.query.get(blog_id)
-    blogs = Blog.query.all()
+    blog_id = request.args.get('id')
+    if blog_id == None:
+        blogs = Blog.query.all()
+        return render_template('blog.html',blogs=blogs, title='Build-A-Blog')
+    else:
+        blog = Blog.query.get(blog_id)
+        return render_template('new-post.html',blog=blog, title='Blog Entry')
+    
     body = blog.body
     title = blog.title
     return render_template('new-post.html', title=blog_title, body=blog.body)
@@ -80,6 +86,7 @@ def viewblog():
 def index():
     encoded_error = request.args.get("error")
     return render_template('blog.html', bloglist=get_bloglist(), error=encoded_error and cgi.escape(encoded_error, quote=True))
+    #return redirect('/blog')
 
 if __name__ == "__main__":
     app.run()
