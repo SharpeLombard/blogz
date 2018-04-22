88d001dc15c0e2897396df5d8d7d4cb041f43e45
Mad Hatter <rebecca.s.lombard@gmail.com>
2018-04-22 18:13:48 -0500

    Setup thru Add User Class

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 import cgi
 
 app = Flask(__name__)
 app.config['DEBUG'] = True     
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:buildablog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogz@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
@@ -14,11 +14,23 @@ class Blog(db.Model):
     title = db.Column(db.String(100))
     body = db.Column(db.String(600))
     deleted = db.Column(db.Boolean)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __init__(self, title, body):
         self.title = title
         self.body = body
+        self.owner = owner
         self.deleted = False
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref='owner')
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
 
     #def __repr__(self):
      #   return '<Blog %r>' % self.blog_title
@@ -88,6 +100,67 @@ def index():
     #encoded_error = request.args.get("error")
     #return render_template('blog.html', bloglist=get_bloglist(), error=encoded_error and cgi.escape(encoded_error, quote=True))
     return redirect('/blog')
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            session['username'] = username
+            return redirect('/newpost')
+        if user and user.password != password:
+            flash()
+            return redirect('/newpost')
+    return render_template('login.html')
+
+@app.route('/logout')
+
+@app.before_request
+
+@app.route('/signup', methods=['POST'])
+def validate_inputs():
+
+    username = request.form['username']
+    password = request.form['password']
+    verify_password = request.form['verify_password']
+    username_error = ''
+    password_error = ''
+    verify_password_error = ''
+
+    if len(username) < 3 or len(username) > 20:
+        username_error = 'Username must be 3-20 characters'
+        username = ''
+        password = ''
+        verify_password = ''
+    elif ' ' in username:
+        username_error = 'Username cannot contain spaces'
+        username = ''
+        password = ''
+        verify_password = ''
+    else:
+        username = username
+
+    if len(password) < 3 or len(password) > 20:
+        password_error = 'Password must be 3-20 characters'
+        password = ''
+        verify_password = ''
+    else:
+        password = password
+
+    if password != verify_password:
+        verify_password_error = 'Passwords do not match!'
+        verify_password = ''
+        password = ''
+
+    if not username_error and not password_error and not verify_password_error:
+        return redirect("/newpost")
+    else:
+        return render_template('signup.html',username=username, password=password, 
+        username_error=username_error, password_error=password_error, verify_password=verify_password, 
+        verify_password_error=verify_password_error, email=email,
+        email_error=email_error)
 
 if __name__ == "__main__":
     app.run()
